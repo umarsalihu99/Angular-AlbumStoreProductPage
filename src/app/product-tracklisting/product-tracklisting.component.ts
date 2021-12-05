dd4d9c1c254a42a17fb07a694a0bb5e06bd5d184
Umar <81313320+umarsalihu99@users.noreply.github.com>
2021-12-05 14:58:38 +0100

    Completed module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
+import { Album } from '../album';
+
 @Component({
   selector: 'app-product-tracklisting',
   templateUrl: './product-tracklisting.component.html',
@@ -7,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  constructor() { }
+  albumInfo: Album;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(responce => 
+      this.albumInfo = responce);
   }
 
 }
