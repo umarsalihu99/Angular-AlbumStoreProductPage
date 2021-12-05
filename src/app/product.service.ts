66b14c63d5d870d1e454716262ccede5daf16aaf
Umar <81313320+umarsalihu99@users.noreply.github.com>
2021-12-05 13:02:55 +0100

    Completed Module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,10 +10,10 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
-
+  
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((Response) => 
-    Response.json()); 
+    return this._http.get(this._albumUrl).map((response) => 
+    response.json()); 
   }
 
 }
