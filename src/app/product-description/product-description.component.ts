6145b8fac75bfbf1a3483d8caa432a7bb2aa2a78
Umar <81313320+umarsalihu99@users.noreply.github.com>
2021-12-05 13:54:01 +0100

    Completed Module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Album } from 'app/album';
-
 import { ProductService } from '../product.service';
+
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
